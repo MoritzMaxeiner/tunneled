c3809ad48ee70339005d161076c7f8c42f83790e
Moritz Maxeiner <moritz@ucworks.org>
2015-07-28 21:46:41 +0200

    Merge FileLock class into tunneled module

--- tunneled.py
+++ tunneled.py
@@ -2,22 +2,66 @@
 
 import os, json, subprocess, enum, sys, pwd, time, argparse, signal;
 
-import FileLock;
+import butter.inotify as inotify;
 
-def drop_sudo_to_suid():
-	try:
-		pwdEntry = pwd.getpwnam(os.environ['SUDO_USER']);
-	except KeyError:
-		sys.exit('Sudo user not found in password database');
-	# Drop real, effective, and saved group id
-	os.setresgid(pwdEntry.pw_gid, pwdEntry.pw_gid, pwdEntry.pw_gid);
-	if os.getresgid() != (pwdEntry.pw_gid, pwdEntry.pw_gid, pwdEntry.pw_gid):
-		sys.exit('Could not properly drop group id');
-	# Drop real, keep effective, and ensure saved equals effective group id
-	(_, euid, _) = os.getresuid();
-	os.setresuid(pwdEntry.pw_uid, euid, euid);
-	if os.getresuid() != (pwdEntry.pw_uid, euid, euid):
-		sys.exit('Could not properly drop user id');
+class FileLock:
+
+	def acquire(self, blocking=True, timeout=-1):
+		if timeout < -1:
+			raise ValueError('timeout value must be strictly positive');
+		timeout = int(timeout);
+		while True:
+			try:
+				self.atomic_file = open(self.filename, 'x');
+				return True;
+			except FileExistsError:
+				if not blocking or timeout == 0:
+					self.atomic_file = None;
+					return False;
+				time_before_wait = time.time();
+				inotify = inotify.Inotify();
+				try:
+					wd = inotify.watch(self.filename, inotify.IN_DELETE_SELF);
+					inotify.wait(timeout=(timeout if timeout >= 0 else None));
+				except TimeoutError:
+					pass;
+				finally:
+					inotify.close();
+				if timeout > 0:
+					timeout = int(max(0, timeout - abs(time.time() - time_before_wait)));
+
+	def release(self):
+		try:
+			if self.atomic_file:
+				self.atomic_file.close();
+			os.remove(self.filename);
+			self.atomic_file = None;
+		except OSError:
+			tb = sys.exc_info()[2];
+			raise RuntimeError('Could not release FileLock "' + self.filename + '"').with_traceback(tb);
+
+	def __init__(self, filename):
+		self.filename = filename;
+		self.atomic_file = None;
+
+	def __del__(self):
+		try:
+			if self.atomic_file:
+				self.atomic_file.close();
+			os.remove(self.filename);
+			self.atomic_file = None;
+		except FileNotFoundError:
+			return;
+		except OSError:
+			tb = sys.exc_info()[2];
+			raise RuntimeError('Could not release FileLock "' + self.filename + '"').with_traceback(tb);
+
+	def __enter__(self):
+		return self.acquire();
+
+	def __exit__(self, exception_type, exception_val, trace):
+		self.release();
+		return True;
 
 class OpenVPNConnection:
 	def __init__(self, name, state='/tmp/.tunneled-state', lock='/tmp/.tunneled-lock'):
@@ -40,7 +84,7 @@ class OpenVPNConnection:
 		self.__acquireOrRelease(False);
 
 	def __acquireOrRelease(self, acquire):
-		with FileLock.FileLock(self.lock):
+		with FileLock(self.lock):
 			(ruid, euid, suid) = os.getresuid();
 
 			try:
@@ -79,6 +123,21 @@ class OpenVPNConnection:
 			# Lose state before releasing lock
 			state = None;
 
+def drop_sudo_to_suid():
+	try:
+		pwdEntry = pwd.getpwnam(os.environ['SUDO_USER']);
+	except KeyError:
+		sys.exit('Sudo user not found in password database');
+	# Drop real, effective, and saved group id
+	os.setresgid(pwdEntry.pw_gid, pwdEntry.pw_gid, pwdEntry.pw_gid);
+	if os.getresgid() != (pwdEntry.pw_gid, pwdEntry.pw_gid, pwdEntry.pw_gid):
+		sys.exit('Could not properly drop group id');
+	# Drop real, keep effective, and ensure saved equals effective group id
+	(_, euid, _) = os.getresuid();
+	os.setresuid(pwdEntry.pw_uid, euid, euid);
+	if os.getresuid() != (pwdEntry.pw_uid, euid, euid):
+		sys.exit('Could not properly drop user id');
+
 def main():
 	if "SUDO_USER" in os.environ.keys():
 		drop_sudo_to_suid();
@@ -116,3 +175,4 @@ def main():
 
 if __name__ == '__main__':
 	main();
+
