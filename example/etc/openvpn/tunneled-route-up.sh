84c6463ac498eab37fd0c8a7919299a168d449c1
Moritz Maxeiner <moritz@ucworks.org>
2017-01-22 19:22:51 +0100

    Misc

--- example/etc/openvpn/tunneled-route-up.sh
+++ example/etc/openvpn/tunneled-route-up.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#! /bin/sh
 
 # Which VPN connection to setup for
 VPN="$(basename ${config} .conf)"
 
 # Clear all routes on the vpn routing table
 # (this is to make sure there isn't any crap left over from previous vpn connection)
-/sbin/ip route flush table "${VPN}"
+ip route flush table "${VPN}"
 
 # Add default gateway to vpn routing table
-/sbin/ip route add default via "${route_vpn_gateway}" dev "${dev}" table "${VPN}"
+ip route add default via "${route_vpn_gateway}" dev "${dev}" table "${VPN}"
 
 # Set the reverse path filter to "loose" for the tunnel interface
-/sbin/sysctl net.ipv4.conf."${dev}".rp_filter=2
+sysctl net.ipv4.conf."${dev}".rp_filter=2
 
